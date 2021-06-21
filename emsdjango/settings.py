81bc03d4c90d94d3f011d51cda29a9bbbeb2042a
RUPAYAN10 <86109345+RUPAYAN10@users.noreply.github.com>
2021-06-22 02:00:31 +0530

    dsd

--- emsdjango/settings.py
+++ emsdjango/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'f8gi_3dlg#+hgsm5c+ozz)$j0bpazo!^gqc4o9#l(dcoymwbo%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','employee-management-system-bcs.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,18 +117,18 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
+#  https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#  import os
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#  # Static files (CSS, JavaScript, Images)
+#  # https://docs.djangoproject.com/en/1.9/howto/static-files/
+#  STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
