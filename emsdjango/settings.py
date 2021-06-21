019a2d23db9f430b08b623726ba1e552492c3056
RUPAYAN10 <86109345+RUPAYAN10@users.noreply.github.com>
2021-06-21 21:50:02 +0530

    adding

--- emsdjango/settings.py
+++ emsdjango/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'f8gi_3dlg#+hgsm5c+ozz)$j0bpazo!^gqc4o9#l(dcoymwbo%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
--- emsdjango/urls.py
+++ emsdjango/urls.py
@@ -2,12 +2,21 @@
 from django.contrib import admin
 from django.urls import path
 from ems import views
+from django.conf.urls import url
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
+    
      path('emp', views.emp),
     path('', views.emp),
     path('show', views.show),
     path('edit/<int:id>', views.edit),
     path('update/<int:id>', views.update),
     path('delete/<int:id>', views.delete),
+    path('admin/', admin.site.urls),
+    
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
